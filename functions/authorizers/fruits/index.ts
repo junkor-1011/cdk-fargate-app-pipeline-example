c4c3091069330b92881c085c963cf917b8ab65d0
junkor-1011 <tomoyukizxy@gmail.com>
2022-08-14 00:48:22 +0900

    feat: add verify token & claim example

--- functions/authorizers/fruits/index.ts
+++ functions/authorizers/fruits/index.ts
@@ -1,13 +1,38 @@
-import type {
-  APIGatewayAuthorizerEvent,
-  // APIGatewayAuthorizerHandler,
-  APIGatewayAuthorizerResult,
-} from 'aws-lambda';
+import 'source-map-support/register';
 
-import { generatePolicy } from '../lib/utils';
+import type { APIGatewayAuthorizerEvent, APIGatewayAuthorizerResult } from 'aws-lambda';
 
-/* eslint-disable-next-line @typescript-eslint/require-await */
-export const lambdaHandler = async (event: APIGatewayAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
-  const res = generatePolicy('', 'Deny', event.methodArn);
-  return res;
+import { generatePolicy, verifyToken } from '../lib/utils';
+
+/**
+ * modified APIGatewayAuthorizerEvent
+ *
+ * add authorizationToken
+ */
+type Event = APIGatewayAuthorizerEvent & {
+  authorizationToken: string;
+};
+
+export const lambdaHandler = async (event: Event): Promise<APIGatewayAuthorizerResult> => {
+  if (event.type !== 'TOKEN') {
+    console.log(`expected authorization type is TOKEN, got ${event.type}`);
+    return generatePolicy('', 'Deny', event.methodArn);
+  }
+  const token = event.authorizationToken;
+
+  let principalId: string;
+  try {
+    const tokenInfo = await verifyToken(token);
+    const fruits = tokenInfo.customKey3.split(' ').filter((x) => !!x);
+    console.log("[INFO]user's fruits:", fruits);
+    if (!fruits.includes('melon')) {
+      console.log('user do not have "melon".');
+      return generatePolicy('', 'Deny', event.methodArn);
+    }
+    principalId = tokenInfo['cognito:username']; // TMP
+  } catch (err) {
+    console.log(`failed to verify token. error:`, err);
+    return generatePolicy('', 'Deny', event.methodArn);
+  }
+  return generatePolicy(principalId, 'Allow', event.methodArn);
 };
