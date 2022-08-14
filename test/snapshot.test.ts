a0d61bf62dd12805f74236804e8cfe42810c528a
junkor-1011 <tomoyukizxy@gmail.com>
2022-08-14 23:24:20 +0900

    test(snapshot): add pipelines' snapshot test

--- test/snapshot.test.ts
+++ test/snapshot.test.ts
@@ -1,9 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { BackendStack } from '../lib/backend-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 describe('snapshot test', () => {
-  it("TestApp Stack's Snapshot test", () => {
+  it("PipelineStack's Snapshot test", () => {
+    const app = new cdk.App();
+    const stack = new PipelineStack(app, 'TestAppStack');
+    const template = Template.fromStack(stack).toJSON();
+
+    expect(template).toMatchSnapshot();
+  });
+  it("BackendStack's Snapshot test", () => {
     const app = new cdk.App();
     const stack = new BackendStack(app, 'TestAppStack');
     const template = Template.fromStack(stack).toJSON();
