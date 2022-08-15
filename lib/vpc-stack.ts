43b34701efe854bfa2545c9e745596edd17fccc9
junkor-1011 <tomoyukizxy@gmail.com>
2022-08-15 21:26:01 +0900

    feat(ssm): vpcid

--- lib/vpc-stack.ts
+++ lib/vpc-stack.ts
@@ -1,6 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
+
+const stageNames = {
+  dev: 'Dev',
+  prod: 'Prod',
+};
 
 export class VpcStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -44,6 +50,18 @@ export class VpcStack extends Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
+    });
+
+    // SSM ParameterStore
+    new ssm.StringParameter(this, 'vpcid-dev', {
+      parameterName: `/TESTAPP/${stageNames.dev}/VPC_ID`,
+      stringValue: vpcDev.vpcId,
+      type: ssm.ParameterType.STRING,
+    });
+    new ssm.StringParameter(this, 'vpcid-prod', {
+      parameterName: `/TESTAPP/${stageNames.prod}/VPC_ID`,
+      stringValue: vpcProd.vpcId,
+      type: ssm.ParameterType.STRING,
     });
   }
 }
